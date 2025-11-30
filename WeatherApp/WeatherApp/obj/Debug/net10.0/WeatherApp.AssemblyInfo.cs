08820e92a044007ac30dc6e3852f6d38c0ce89b0
Nigel Ang <ajknigel360@gmail.com>
2025-11-30 01:09:30 +0000

    Development 0.2.17

--- WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36bf8f1d4264a80c80315f96f163de9d0a0e5eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef34c28a64b8f562b4b39ab1825e6a2f4905eda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
