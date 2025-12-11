b079480e8ac391e03d825393ef7d76fdf17028f6
Nigel Ang <ajknigel360@gmail.com>
2025-12-11 16:31:18 +0000

    Development 0.4.0

---
+++ FullStack/frontend/Models/Product.cs
@@ -0,0 +1,4 @@
+namespace YourBlazorWasmApp.Models;
+
+// This C# record must match the structure of the JSON from your API.
+public record Product(int Id, string Name);
--- WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c50f9da6b7cd77ab3cdf7875734fcdcd7ac40ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf34077f53f1db5da8cffeea9c2180d5628b906")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
