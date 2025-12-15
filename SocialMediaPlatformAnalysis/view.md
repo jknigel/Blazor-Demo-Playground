e80d27cba6b4afec013bd784a38548b663335b9d
jknigel <ajknigel360@gmail.com>
2025-12-15 22:12:09 +0800

    Development 0.4.3

---
+++ SocialMediaPlatformAnalysis/view.md
@@ -0,0 +1,6 @@
+Front-end: Send a GET request to the backend
+Back-end: API queries the database for recent posts and returns as JSON array
+
+Data flow: 
+GET request → API → Database query.
+JSON response → Feed rendered on the front-end.
--- WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/WeatherApp/obj/Debug/net10.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b079480e8ac391e03d825393ef7d76fdf17028f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea187259490fe19c7a282f5b24c7a5bda0f8c53")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
